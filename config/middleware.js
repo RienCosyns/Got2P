960bf0450c6d43a52a2f105ca3f006c49d745f1f
[rien.cosyns] <[rien.cosyns@gmail.com]>
2017-06-22 12:21:25 +0200

    express session middleware added

--- config/middleware.js
+++ config/middleware.js
@@ -2,14 +2,22 @@
 // middleware
 // ***********************************
 const express = require("express");
-const middleware = express();
+const middleware = express.Router();
 const bodyParser = require("body-parser");
 const path = require("path");
+const session = require("express-session");
 
 middleware.use(bodyParser.json());
 middleware.use(bodyParser.urlencoded({extended: false}));
 
 // Set public folder
 middleware.use(express.static(path.join(__dirname, "../public")));
+
+// Express session middleware
+middleware.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}));
 
 module.exports = middleware;
